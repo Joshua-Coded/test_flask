abab9fdecc518ec696bbc72333a0d3e9122f1f82
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-23 15:17:24 +0100

    for loop in flask

--- blog.py
+++ blog.py
@@ -4,10 +4,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Joshua Alana',
+        'title': 'Blog Post',
+        'content': 'First Post Content',
+        'date_posted': 'February 23 2024'
+        },
+        {
+        'author': 'Alana',
+        'title': 'Twitter Post',
+        'content': 'First Post Content',
+        'date_posted': 'February 23 2024'
+        }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
