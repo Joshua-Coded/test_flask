44f9dc856664d5fc30da257c0f24c787aa39f750
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-24 03:33:31 +0100

    login and registration form

--- blog.py
+++ blog.py
@@ -1,8 +1,8 @@
-
-""" Script that runs a flask application"""
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '1bce43da0fa199def6acc2f7dcfb6d2e'
 
 posts = [
     {
@@ -28,6 +28,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.tml', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
