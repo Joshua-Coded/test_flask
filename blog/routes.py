34be31715f4dc83c108a2ea11722931d88719e55
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-02 19:59:08 +0100

    updating users

--- blog/routes.py
+++ blog/routes.py
@@ -72,5 +72,14 @@ def logout():
 @login_required
 def account():
     form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for('account.html'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
