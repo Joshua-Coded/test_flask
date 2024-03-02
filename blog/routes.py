d08c48a183c699acac352ec22cf22ee78fd56951
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-02 21:12:01 +0100

    thumbnail

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from blog import app, db, bcrypt
 from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -75,7 +76,13 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
     return picture_fn
 
 @app.route('/account', methods=['GET', 'POST'])
