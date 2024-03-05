f31c585a26b692d8a2f5271e53d62f37c97d4a82
joshbam-joshwilly <j.alana@alustudent.com>
2024-03-05 11:40:37 +0100

    user page post

--- blog/routes.py
+++ blog/routes.py
@@ -27,7 +27,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -158,3 +158,11 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your Post has been deleted!", "success")
     return redirect(url_for('home'))
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
